bcfdf2b0eadd5ada4a4cc011d8764d09ec4f477d
Austin Kabanda <96333691+xen-py@users.noreply.github.com>
2023-01-14 18:19:54 -0700

    Add files via upload

--- manager.js
+++ manager.js
@@ -1,9 +1,9 @@
 
 let Editors = {
     1 : "future editors",
-    2 : "Austin, Isabella, Ivn",
+    2 : "Austin, Avni, Isabella, Shaneel",
     3 : "future editors",
-    4 : "future editors"
+    4 : "Amon, Kai"
 }
 
 
@@ -17,7 +17,7 @@ function getArticleNumber() {
     console.log(article)
 }
 
-
+        
 getArticleNumber()
 
 function changeTitles (){
@@ -53,13 +53,14 @@ function changeTitles (){
         case 4:
             title.textContent = 'Current Events'
             genre.textContent = 'Current Events'
-            articleTitle.textContent = 'to be replaced'
+            articleTitle.textContent = 'World Cup'
             break;
     }
 
-    articleTitle.style.fontSize = '25px'
+    articleTitle.style.fontSize = '30px'
     articleTitle.style.fontFamily = '"Montserrat", sans-serif'
-    articleTitle.style.color = '#1D71F5'
+    articleTitle.style.color = 'darkblue'
+    articleTitle.style.fontWeight = 'bold'
 
 
 }
@@ -122,11 +123,18 @@ function content(){
             //main content
             document.writeln('<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus officiis nemo explicabo recusandae earum ad distinctio aperiam voluptates saepe, nesciunt accusamus quasi ex nobis expedita ipsum doloribus ratione vel possimus?</p>')
             document.writeln('<div class="spacer"></div>')
+            document.write('<h2>Best Hangout Spots</h2>')
+            document.writeln('<p>Edmonton has a wide variety of great places to hang out with friends, depending on your interests. For those who enjoy the outdoors, the River Valley Parks system offers miles of hiking and biking trails, as well as parks, picnic areas and playgrounds. Whyte Avenue is another popular hangout spot for friends, with a variety of shops, restaurants, and bars. If you\'re interested in arts and culture, the Royal Alberta Museum is an excellent destination for spending time with friends, featuring exhibits on the history and culture of Alberta. The Old Strathcona district is another popular spot with a great atmosphere, home to a variety of restaurants, bars, and shops, as well as a farmer\'s market and several festivals throughout the year. Another fun spot for friends to hangout would be to explore the food scene, trying out different and unique restaurants or even food trucks on Whyte Avenue or 124 street.</p>')
+            document.writeln('<div class="spacer"></div>')
             document.writeln("<p class = 'date' >Editors: "+Editors[2]+'</p>')
+            break;
+        case 4:
+            document.writeln('<p>The 2022 World Cup was one for the ages. The World Cup began with some of the most bizarre matches, such as Argentina losing to Saudi Arabia and Japan defeating European giants Spain and Germany in blue lock kits. This year\'s World Cup had fans crying and cheering, and many supposedly "easy games" for some countries ended with the underdogs winning. The World Cup finals were one of, if not the, craziest matches in football history. Argentina had the lead 2-0 with Messi and Di Maria both scoring until the 80th minute when Kylian Mbappe scored a penalty, and then in the next minute, he scored another goal. During extra time, Messi was able to score one more goal, but shortly after Kylian Mbappe scored his third, making it a hat trick. It ended up being 3-3 and the game went to penalties. Argentina scored four times when France only scored twice, which finally closed the goat discussion. Messi is officially the Goat.</p>')
+            document.writeln('<div class="spacer"></div>')
+            document.writeln("<p class = 'date' >Editors: "+Editors[4]+'</p>')
             break;
 
     }
-    document.write('')
 
 }
 
